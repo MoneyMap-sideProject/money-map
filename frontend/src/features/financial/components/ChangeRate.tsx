258d9bcfff3a845673724df7e73705bdac287df8
Seonu <seonup95@gmail.com>
2025-03-18 16:17:29 +0900

    refactor: 폴더 구조 변경

---
+++ frontend/src/features/financial/components/ChangeRate.tsx
@@ -0,0 +1,103 @@
+import BottomFixedContainer from '../../../shared/ui/BottomFixedContainer';
+import InputLabel from '../../../shared/ui/InputLabel';
+import { PageTitle } from '../../../shared/ui/PageTitle';
+import ProgressButton from '../../../shared/ui/ProgressButton';
+import Footer from '../../../widgets/footer/Footer';
+import {
+  FinancialButtonWrapper,
+  FinancialForm,
+  FinancialFormItem,
+  FinancialInput,
+  FinancialSection,
+} from '../ui/Financial';
+import { useForm } from 'react-hook-form';
+
+type FormInput = Record<
+  'salaryGrowthRate' | 'inflationRate' | 'investmentGrowthRate',
+  number
+>;
+
+type Props = FormInput & {
+  totalStep: number;
+  currentStep: string;
+  currentStepIndex: number;
+  goNext: () => void;
+  setFunnelContext: (context: FormInput) => void;
+};
+
+export function ChangeRate({
+  salaryGrowthRate,
+  inflationRate,
+  investmentGrowthRate,
+  currentStepIndex,
+  totalStep,
+  goNext,
+  setFunnelContext,
+}: Props) {
+  const { register, handleSubmit, getValues } = useForm({
+    defaultValues: {
+      salaryGrowthRate,
+      inflationRate,
+      investmentGrowthRate,
+    },
+  });
+
+  const updateFinancialFormState = () => {
+    const values = getValues();
+
+    setFunnelContext(values);
+    goNext();
+  };
+
+  return (
+    <FinancialSection onSubmit={handleSubmit(updateFinancialFormState)}>
+      <header>
+        <PageTitle>변화율</PageTitle>
+      </header>
+
+      <FinancialForm>
+        <FinancialFormItem>
+          <div>
+            <InputLabel htmlFor="salaryGrowthRate">연봉 상승률</InputLabel>
+            <FinancialInput
+              type="number"
+              id="salaryGrowthRate"
+              {...register('salaryGrowthRate')}
+            />
+          </div>
+          <div>
+            <InputLabel htmlFor="inflationRate">물가 상승률</InputLabel>
+            <FinancialInput
+              type="number"
+              id="inflationRate"
+              {...register('inflationRate')}
+            />
+          </div>
+        </FinancialFormItem>
+        <FinancialFormItem>
+          <div>
+            <InputLabel htmlFor="investmentGrowthRate">투자 상승률</InputLabel>
+            <FinancialInput
+              type="number"
+              id="investmentGrowthRate"
+              {...register('investmentGrowthRate')}
+            />
+          </div>
+        </FinancialFormItem>
+
+        <BottomFixedContainer>
+          <FinancialButtonWrapper>
+            <ProgressButton
+              type="submit"
+              total={totalStep}
+              step={currentStepIndex + 1}
+            >
+              다음
+            </ProgressButton>
+          </FinancialButtonWrapper>
+          <Footer />
+        </BottomFixedContainer>
+      </FinancialForm>
+    </FinancialSection>
+  );
+}
