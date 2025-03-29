8c9d7553786ba1dd1d6997865499002d6e6fb8f8
Seonu <seonup95@gmail.com>
2025-03-29 20:48:38 +0900

    feat: 요청 path 변경 및 요청 함수 추가

--- frontend/src/shared/api/user/index.ts
+++ frontend/src/shared/api/user/index.ts
@@ -2,13 +2,17 @@ import { SignUpRequestBody, LoginRequestBody } from './type';
 import instance from './instance';
 
 export const signUp = async (body: SignUpRequestBody) => {
-  return await instance.post('', body);
-};
-
-export const getProfile = async () => {
-  return await instance.get('');
+  return await instance.post('/register', body);
 };
 
 export const login = async (body: LoginRequestBody) => {
   return await instance.post('/login', body);
+};
+
+export const logout = async () => {
+  return await instance.get('/logout');
+};
+
+export const getProfile = async () => {
+  return await instance.get('/profile');
 };
