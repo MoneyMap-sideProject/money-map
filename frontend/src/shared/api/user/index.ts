67d7aaad01fbfe8a1d1d6e87161e221507a0de89
Seonu <seonup95@gmail.com>
2025-04-05 14:03:36 +0900

    refactor: instance 생성시 path 제외

--- frontend/src/shared/api/user/index.ts
+++ frontend/src/shared/api/user/index.ts
@@ -2,17 +2,17 @@ import { SignUpRequestBody, LoginRequestBody } from './type';
 import instance from './instance';
 
 export const signUp = async (body: SignUpRequestBody) => {
-  return instance.post('/register', body);
+  return instance.post('/users/register', body);
 };
 
 export const login = async (body: LoginRequestBody) => {
-  return instance.post('/login', body);
+  return instance.post('/users/login', body);
 };
 
 export const logout = async () => {
-  return instance.get('/logout');
+  return instance.get('/users/logout');
 };
 
 export const getProfile = async () => {
-  return instance.get('/profile');
+  return instance.get('/users/profile');
 };
