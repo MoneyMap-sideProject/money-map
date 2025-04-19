cbcbf10436675efcdc21d943f749b61382514a06
Seonu <seonup95@gmail.com>
2025-04-19 16:53:39 +0900

    fix: 변경된 Icon import 방식 적용

--- frontend/src/features/logout/LogoutButton.tsx
+++ frontend/src/features/logout/LogoutButton.tsx
@@ -2,9 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 import styled from 'styled-components';
 import { queryKey } from '@/shared/api/user/queryKey';
 import { logout } from '@/shared/api/user';
-import OutIcon from '@/assets/svgs/out.svg?react';
 import { useNavigate } from '@tanstack/react-router';
 import { toast } from 'react-toastify';
+import Icon from '@/shared/ui/Icon';
 
 export default function LogoutButton() {
   const { refetch } = useQuery({
@@ -28,7 +28,7 @@ export default function LogoutButton() {
 
   return (
     <Button type="button" onClick={_logout}>
-      <OutIcon />
+      <Icon type="out" width="20px" height="20px" />
       로그아웃
     </Button>
   );
@@ -40,6 +40,7 @@ const Button = styled.button`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  gap: 4px;
   color: ${(props) => props.theme.colors.grayDark};
   font-size: 12px;
   font-weight: 400;
